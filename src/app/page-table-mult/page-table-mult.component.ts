5f4e4839f5c511cbd8c6a3e60d86b0890b4de7ec
Baptiste <tabarbaptiste@gmail.com>
2023-02-20 00:23:06 +0100

    Mise à jour du site

--- src/app/page-table-mult/page-table-mult.component.ts
+++ src/app/page-table-mult/page-table-mult.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 
+// Le décorateur @Component permet de définir les métadonnées du composant
 @Component({
-  selector: 'app-page-table-mult',
-  templateUrl: './page-table-mult.component.html',
-  styleUrls: ['./page-table-mult.component.css']
+  selector: 'app-page-table-mult',  // Le sélecteur est utilisé pour déterminer où le composant sera inséré dans le DOM
+  templateUrl: './page-table-mult.component.html',  // Le templateUrl est utilisé pour déterminer où se trouve le template du composant
+  styleUrls: ['./page-table-mult.component.css']  // Le styleUrls est utilisé pour déterminer où se trouvent les fichiers de styles du composant
 })
+
+// La classe PageTableMultComponent contient la logique de notre composant
 export class PageTableMultComponent {
 
-  n: number = 2;
+  n: number = 2;  // Initialisation de la variable n à 2
 
+  // La fonction updateN est appelée lorsque l'utilisateur modifie la valeur de l'input
   updateN(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    this.n = +inputElement.value;
+    const inputElement = event.target as HTMLInputElement;    // On récupère l'élément input
+    this.n = +inputElement.value;    // On convertit la valeur de l'input en nombre et on l'assigne à la variable n
   }
 
 }
--- src/app/table-mult-list/table-mult-list.component.ts
+++ src/app/table-mult-list/table-mult-list.component.ts
@@ -6,24 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./table-mult-list.component.css']
 })
 export class TableMultListComponent {
+
+  // Tableau des nombres à afficher
   numbers: number[] = [];
+
+  // Nombre de tables à afficher
   numberOfTables = 10;
 
+  // Constructeur de la classe
   constructor() {
-    this.updateNumbers();
+    this.updateNumbers(); // Initialise le tableau des nombres
   }
 
+  // Met à jour le tableau des nombres à afficher
   updateNumbers() {
-    this.numbers = Array.from({length: this.numberOfTables}, (_, i) => i+1);
+    this.numbers = Array.from({length: this.numberOfTables}, (_, i) => i+1); // Génère le tableau
   }
 
+  // Génère un tableau de nombres allant de 'start' à 'end'
   range(start: number, end: number): number[] {
-    return Array.from({length: (end - start + 1)}, (_, i) => start + i);
+    return Array.from({length: (end - start + 1)}, (_, i) => start + i); // Génère le tableau
   }
 
+  // Met à jour le nombre de tables à afficher
   updateNumberOfTables(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    this.numberOfTables = +inputElement.value;
-    this.updateNumbers();
+    const inputElement = event.target as HTMLInputElement; // Récupère l'élément input qui a déclenché l'événement
+    this.numberOfTables = +inputElement.value; // Récupère la valeur de l'input et la convertit en nombre
+    this.updateNumbers(); // Met à jour le tableau des nombres à afficher
   }
 }
